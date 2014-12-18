4ca0b226b34117b255b18bba414d96a14b3c437a
Jail81 <jail81@gmail.com>
2014-12-18 15:28:43 +0100

    Fixes in the todo section.

--- angular-webapp/app/js/todos/todosCtrl.js
+++ angular-webapp/app/js/todos/todosCtrl.js
@@ -1,15 +1,18 @@
 angular.module('todos')
-    .controller('todosCtrl', function ($scope) {
+    .controller('todosCtrl', ['$scope', 'i18nService', function ($scope, i18nService) {
         $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
 
         $scope.addTodo = function () {
-            if (angular.isDefined() && $scope.todo != '') {
+            if (angular.isDefined($scope.todo) && $scope.todo != '') {
                 $scope.todos.push($scope.todo);
                 $scope.todo = '';
+                $scope.isEmptyTodo = false;
+            } else {
+                $scope.isEmptyTodo = true;
             }
         };
 
         $scope.removeTodo = function (index) {
             $scope.todos.splice(index, 1);
         };
-    });
+    }]);
